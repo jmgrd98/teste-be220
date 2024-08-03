2ebf074c29723974bc44386c541c46c8720c01cc
joaodantasdev <jmgrd98@gmail.com>
2024-08-02 22:09:17 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true
 };
+
+export const API_KEY="AIzaSyA5VRqDKePcJcPZGxbV49TZa-DJa0f4mD4"
+export const AUTH_DOMAIN="teste-be220-48aca.firebaseapp.com"
+export const PROJECT_ID="teste-be220-48aca"
+export const STORAGE_BUCKET="teste-be220-48aca.appspot.com"
+export const MESSAGING_SENDER_ID="974195830996"
+export const APP_ID="1:974195830996:web:f3ae34622056b846c84b41"
+export const MEASUREMENT_ID="G-94KGP1FBP5"
